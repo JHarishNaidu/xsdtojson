d15d0977383bfe7aafe007ffae24735819ba1309
JHarishNaidu <harishnaidujarra@gmail.com>
2020-05-18 15:32:34 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 from setuptools import setup
 
 install_requirements = parse_requirements('./requirements.txt', session=False)
@@ -8,8 +11,8 @@ setup(
     name='xsdtojson',
     version='0.1',
     description='Convert XSD to JSON Schema',
-    author='Ben Scott',
-    author_email='ben@benscott.co.uk',
+    author='Harish Naidu',
+    author_email='harishnaidujarra@gmail.com',
     packages=['xsdtojson'],
     install_requires=requirements,
     entry_points={
